eacd938464fbe982b9941eabf783e7c3486bc2da
Hao Li <haoli@alauda.io>
2020-11-04 10:01:01 +0800

    Change footer link href

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,6 +4,6 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">chaos1ee</a>
+    ©<a href="https://github.com/chaos1ee">chaos1ee</a>
   </footer>
 )
